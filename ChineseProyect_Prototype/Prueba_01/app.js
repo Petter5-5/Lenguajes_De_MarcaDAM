ba3a6461effd7814919b25669975397afd3062e1
Petter5-5 <yeraytrejosanchez@gmail.com>
2025-12-03 20:29:12 +0100

    03/12/2025

--- ChineseProyect_Prototype/Prueba_01/app.js
+++ ChineseProyect_Prototype/Prueba_01/app.js
@@ -1,9 +1,11 @@
 const opciones = document.querySelectorAll('.opcion');
 const contenido = document.getElementById('contenido');
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
+const menu2 = document.getElementById("menu2");
+const volver = document.getElementById("volver");
+const titulo = document.getElementById("titulo")
 
-
-
+//slide
 opciones.forEach(op => {
     op.addEventListener('click', () => {
         const color = op.dataset.color;
@@ -12,10 +14,50 @@ opciones.forEach(op => {
 
         // Cambiar fondo a color sólido
         document.body.style.backgroundImage = "none";
-        document.body.style.backgroundColor = color;
+        document.body.style.backgroundColor = "rgba(187, 21, 199, 0.8)";
 
         // Animar el contenido
-        contenido.style.transform = "translateX(150px)";
-        contenido.textContent = `Elegiste: ${op.textContent}`;
+        titulo.textContent = op.textContent;
     });
+});
+
+//Ocultar menu grande
+opciones.forEach(op => {
+    op.addEventListener('click', () => {
+
+        //ocultar menu grande
+        menu.classList.add("oculto");
+        menu.classList.remove("visible");
+
+        //mostrar menu pequeño
+        setTimeout(() => {
+            menu2.classList.remove("oculto");
+            menu2.classList.add("visible");
+        }, 300);
+    });
+});
+
+//Ocultar menu pqueño y volver al menu
+volver.addEventListener('click', () => {
+
+    //cargar imagen background
+    document.body.style.backgroundImage = "url(img/chengdu-china.jpg)";
+    document.body.style.backgroundColor = "black";
+
+    //Ocultar menu pequeño
+    menu2.classList.add("oculto");
+    menu2.classList.remove("visible");
+
+    //quitar el colapsado
+    menu.classList.remove("colapsado");
+
+    //si
+    titulo.textContent = "欢迎";
+
+    setTimeout(() => {
+
+        //Mostrar menu grande
+        menu.classList.remove("oculto");
+        menu.classList.add("visible");
+    }, 300)
 });
